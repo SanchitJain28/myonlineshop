6a4f76378574501b76685996cdc615dd8bfeffe5
SanchitJain28 <mythichuman28@gmail.com>
2024-10-30 14:47:30 +0530

    image Preview

--- myonlineshop/src/components/FileUpload.js
+++ myonlineshop/src/components/FileUpload.js
@@ -15,34 +15,51 @@ import { useNavigate } from 'react-router-dom'
 
 export default function FileUpload() {
   const toast = useToast()
-  const navigate=useNavigate()
-  const productContext=useContext(productAPI)
+  const navigate = useNavigate()
+  const productContext = useContext(productAPI)
+  const [imagePreview, setImagePreview] = useState([]);
   const [file, setFile] = useState([])
   const [productName, setProductName] = useState("")
   const [productCategory, setProductCategory] = useState("")
   const [productPrice, setProductPrice] = useState("")
   const [productDescription, setProductDescription] = useState("")
   const [loader, setLoader] = useState(false)
-  const [progress,setProgress]=useState(0)
-  const emptyFeilds=()=>{
+  const emptyFeilds = () => {
     setProductName("")
     setProductCategory("")
     setProductDescription("")
     setProductPrice("")
   }
-  
+
+  const ProgressBar=()=>{
+
+  }
+
   return (
     <>
-    <Progress progress={progress}  size="lg"  />;
       <p className='text-center text-2xl p-8'>Create a Product</p>
       <div className="flex flex-col justify-center p-12 border m-12">
         <FileInput type="file" name="file" className='my-4 p-2 form-control-file' multiple onChange={(e) => {
+          //Waah chatgpt wah ,kya baat hai URL.createObjectURL helped in image Preview
+          const files = Array.from(e.target.files)
           setFile(e.target.files);
-          setProgress(progress+20)
+          console.log(files)
+          const previewUrls = files.map((file) => URL.createObjectURL(file));
+          setImagePreview(previewUrls); // For preview
+          console.log(previewUrls)
         }} />
+        <div className="flex">
+          {imagePreview.map((e) => {
+            return <>
+              <div className="flex flex-col">
+                <img src={e} className='w-24 mx-2' />
+              </div>
+            </>
+          })}
+        </div>
+
         <Input placeholder='product Name ' className='my-4 p-4 border' onChange={(e) => {
           setProductName(e.target.value)
-          
         }} value={productName}></Input>
         <Select placeholder='Select Category' size='md' onChange={(e) => {
           setProductCategory(e.target.value)
@@ -60,10 +77,10 @@ export default function FileUpload() {
         }} value={productPrice}></Input>
         <Textarea placeholder='product Description ' className='my-4 p-4 border' onChange={(e) => {
           setProductDescription(e.target.value)
-        }} size='xl' value={productDescription}/>
-        <Button onClick={async() => {
+        }} size='xl' value={productDescription} />
+        <Button onClick={async () => {
           setLoader(true)
-          const data =await productContext.createProduct(file,productName,productCategory,productPrice,productDescription)
+          const data = await productContext.createProduct(file, productName, productCategory, productPrice, productDescription)
           setLoader(false)
           emptyFeilds()
           setTimeout(() => {
@@ -78,7 +95,7 @@ export default function FileUpload() {
           })
           console.log(data)
         }} disabled={loader}>Upload   {loader ? <Spinner /> : ""}</Button>
-      
+
       </div>
 
     </>
