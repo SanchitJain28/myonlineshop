c5c1a6025db946f93182a312ec457f0b0c062409
SanchitJain28 <mythichuman28@gmail.com>
2025-05-04 14:15:46 +0530

    fix: Set fixed amount for order creation in create-order endpoint

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -93,7 +93,7 @@ router.post("/api/create-order", verifyUser, async (req, res) => {
     paymentMethod,
   } = req.body;
   const options = {
-    amount: amount, // Amount in paise
+    amount: 100, // Amount in paise
     currency: "INR",
     receipt: "order_rcptid_11",
   };
